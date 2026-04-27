44d71a1d5e6032ee2e369f5308ead62df7c245e5
NischalChenna <chennanischal5000@gmail.com>
2026-04-27 18:33:18 +0530

    fix: restore sync_data in sync log event

--- weavex_core/logging_utils/sdk.py
+++ weavex_core/logging_utils/sdk.py
@@ -98,7 +98,7 @@ class WeavexServicesLogger:
             "vendor_method": vendor_method,
             "vendor_request_id": vendor_req_id,
             # Stringify JSON fields
-            "metadata": json.dumps(metadata or {}),
+            "sync_data": json.dumps(metadata or {}),
             "context": json.dumps(context or {})
         }
         self.logger.log(payload, blocking=True)
