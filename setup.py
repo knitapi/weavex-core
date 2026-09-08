dcacea3d673e534b14a3923a7f0b440a92850b7f
updesh-sb <updesh@getknit.dev>
2026-09-08 18:49:33 +0530

    Changed pysco

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "google-cloud-storage>=2.0.0",
 
         # Database
-        "psycopg2-binary==2.9.9",
+        "psycopg2-binary>=2.9.10",
         "snowflake-connector-python>=4.5.0",
 
         # LangChain
